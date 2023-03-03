3d8a30df5547d88c41b3a2f46b35d69dfdc6ec4a
Er.Mayuresh B. Sankpal <mayureshsankpal9@gmail.com>
2023-03-03 16:02:17 +0530

    Added array request param and db password change.

--- src/main/java/com/example/company/controller/DeptController.java
+++ src/main/java/com/example/company/controller/DeptController.java
@@ -5,10 +5,7 @@ import com.example.company.service.DeptService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +29,12 @@ public class DeptController {
         log.info("Entering into DeptController.getDepartmentByDeptId(@PathVariable(\"dId\") Long deptId). Requested Dept Id - {}", deptId);
         return deptService.getDepartmentByDeptId(deptId);
     }
+
+    @GetMapping("/exp")
+    public String doExp(@RequestParam String[] id){
+        System.out.println(id.length);
+        return "None";
+    }
+
 
 }
